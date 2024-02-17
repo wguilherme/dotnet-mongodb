6b7b46d2914f2e2f8b10f4a8d58bbfe6addbe5cc
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-02-17 18:04:56 -0300

    chore: update git ignore

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Driver;
 
 Console.WriteLine("Hello, World!");
@@ -8,11 +7,9 @@ var client = new MongoClient("mongodb://localhost:27017");
 var database = client.GetDatabase("testdb");
 var collection = database.GetCollection<BsonDocument>("testcollection");
 
-// Clearing the collection (optional)
 await collection.DeleteManyAsync(new BsonDocument());
 Console.WriteLine("Collection cleared.");
 
-// Inserting a document
 var document = new BsonDocument
             {
                 { "name", "Ardalis" },
@@ -23,7 +20,6 @@ var document = new BsonDocument
 await collection.InsertOneAsync(document);
 Console.WriteLine("Document inserted.");
 
-// Reading documents
 var documents = await collection.Find(new BsonDocument()).ToListAsync();
 foreach (var doc in documents)
 {
